dc4b9ed173031d0efbdf73cc943330181d11a62c
rudra2609-06 <rupalthakkar18587@gmail.com>
2025-10-20 10:58:44 +0530

    Add local storage feat and all basic handllings of tracker to git environment

--- script.js
+++ script.js
@@ -1,51 +1,113 @@
-;
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener("DOMContentLoaded", function () {
     var inputDescription = document.getElementById("input-description");
     var inputAmount = document.getElementById("input-amount");
     var inputType = document.getElementById("input-type");
     var addTransactionBtn = document.getElementById("add-btn");
-    //Display Details 
+    var filterBtn = document.getElementById("filter-btn");
     var balancePlaceholder = document.getElementById("temporary-balance-placeholder");
     var incomePlaceholder = document.getElementById("temporary-income-placeholder");
     var expensePlaceholder = document.getElementById("temporary-expense-placeholder");
-    //Transactions Array
-    var transactions = [];
+    var showTransactionDiv = document.getElementById("show-transaction");
+    var transactions = JSON.parse(localStorage.getItem("transactions") || "[]");
+    function noTransactionMessage() {
+        var noTransaction = document.createElement("h3");
+        noTransaction.className = "text-gray-500 text-left font-bold";
+        noTransaction.textContent = "No Transaction";
+        showTransactionDiv.appendChild(noTransaction);
+    }
     function addTransaction() {
         var enteredDescription = inputDescription.value.trim();
         var enteredAmount = parseInt(inputAmount.value);
         var enteredType = inputType.value;
-        console.log("Entered type is: ", enteredType);
+        console.log("Entered type is:", enteredType);
+        if (!enteredDescription || !enteredAmount || !enteredType) {
+            alert("Please Add the required Fields");
+            return;
+        }
         var newTransaction = {
             transactionDescription: enteredDescription,
             transactionAmount: enteredAmount,
             transactionType: enteredType === "income" ? "Income" : "Expense",
+            transactionId: Date.now()
         };
         transactions.push(newTransaction);
+        saveTransaction();
         console.log(transactions);
-        if ((!enteredDescription || !enteredAmount || !enteredType)) {
-            alert("Please Add the required Fields");
-            return;
-        }
-        else {
-            displayTransactionDetails();
-        }
+        displayTransactionDetails();
         inputDescription.value = "";
         inputAmount.value = "";
     }
     function displayTransactionDetails() {
+        showTransactionDiv.innerHTML = "";
         var income = 0;
         var expense = 0;
-        transactions.forEach(function (transaction) {
-            if (transaction.transactionType === "Income") {
-                income += transaction.transactionAmount;
-            }
-            else {
-                expense += transaction.transactionAmount;
+        var transactionList = document.createElement("ul");
+        transactionList.className = "w-full bg-white shadow-md rounded-2xl p-4";
+        var title = document.createElement("h3");
+        title.className = "font-bold text-gray-800 text-xl mb-4";
+        title.textContent = "Transactions";
+        transactionList.appendChild(title);
+        if (transactions.length > 0) {
+            transactions.forEach(function (transaction) {
+                var li = document.createElement("li");
+                li.className = "py-4 px-6 flex items-center justify-between border-b border-gray-200 last:border-none";
+                var desc = document.createElement("h4");
+                desc.className = "font-semibold text-gray-800";
+                desc.textContent = transaction.transactionDescription;
+                var amount = document.createElement("p");
+                amount.className =
+                    transaction.transactionType === "Income"
+                        ? "text-green-600 font-medium"
+                        : "text-red-600 font-medium";
+                amount.textContent = "\u20B9".concat(transaction.transactionAmount);
+                var type = document.createElement("p");
+                type.className = "text-gray-500 italic";
+                type.textContent = transaction.transactionType;
+                var deleteBtn = document.createElement('button');
+                deleteBtn.className = "delete-btn p-3 bg-red-500 rounded-md text-white font-medium cursor-pointer hover:bg-red-400 transition-colors duration-400";
+                deleteBtn.textContent = "Delete";
+                deleteBtn.setAttribute('data-id', transaction.transactionId.toString());
+                li.append(desc, amount, type, deleteBtn);
+                transactionList.appendChild(li);
+                if (transaction.transactionType === "Income") {
+                    income += transaction.transactionAmount;
+                }
+                else {
+                    expense += transaction.transactionAmount;
+                }
+            });
+            showTransactionDiv.appendChild(transactionList);
+        }
+        incomePlaceholder.textContent = "+ \u20B9".concat(income);
+        expensePlaceholder.textContent = "- \u20B9".concat(expense);
+        balancePlaceholder.textContent = "\u20B9".concat(income - expense);
+        //delete functionality
+        showTransactionDiv.addEventListener('click', function (e) {
+            var targetedListEvent = e.target;
+            if (targetedListEvent.tagName === "BUTTON" && targetedListEvent.classList.contains("delete-btn")) {
+                var id_1 = Number(targetedListEvent.getAttribute('data-id'));
+                if (!id_1)
+                    return;
+                transactions = transactions.filter(function (transaction) { return transaction.transactionId !== id_1; });
+                saveTransaction();
+                displayTransactionDetails();
+                if (transactions.length === 0) {
+                    noTransactionMessage();
+                    return;
+                }
             }
         });
-        incomePlaceholder.textContent = "+ ".concat(income);
-        expensePlaceholder.textContent = "".concat(expense);
-        balancePlaceholder.textContent = "".concat(income - expense);
     }
-    addTransactionBtn.addEventListener('click', addTransaction);
+    function saveTransaction() {
+        localStorage.setItem('transactions', JSON.stringify(transactions));
+    }
+    addTransactionBtn.addEventListener("click", addTransaction);
+    filterBtn.addEventListener('click', filterTransactions);
+    //Show No Transaction Message By Default
+    if (transactions.length === 0) {
+        noTransactionMessage();
+    }
+    else {
+        displayTransactionDetails();
+    }
 });
--- script.ts
+++ script.ts
@@ -1,83 +1,147 @@
 interface Transaction {
-	transactionDescription : string,
-	transactionAmount : number,
-	transactionType : "Income" | "Expense"
-};
+	transactionDescription: string;
+	transactionAmount: number;
+	transactionType: "Income" | "Expense";
+	transactionId : number;
+}
 
+document.addEventListener("DOMContentLoaded", () => {
+	const inputDescription = document.getElementById("input-description") as HTMLInputElement;
+	const inputAmount = document.getElementById("input-amount") as HTMLInputElement;
+	const inputType = document.getElementById("input-type") as HTMLSelectElement;
+	const addTransactionBtn = document.getElementById("add-btn") as HTMLButtonElement;
+	const filterBtn = document.getElementById("filter-btn") as HTMLButtonElement;
 
-document.addEventListener('DOMContentLoaded' , () =>  {
+	const balancePlaceholder = document.getElementById("temporary-balance-placeholder") as HTMLParagraphElement;
+	const incomePlaceholder = document.getElementById("temporary-income-placeholder") as HTMLParagraphElement;
+	const expensePlaceholder = document.getElementById("temporary-expense-placeholder") as HTMLParagraphElement;
 
-	const inputDescription = (document.getElementById("input-description") as HTMLInputElement);
-	const inputAmount = (document.getElementById
-	("input-amount") as HTMLInputElement);
-	const inputType = (document.getElementById
-	("input-type") as HTMLSelectElement);
-	const addTransactionBtn = (document.getElementById
-	("add-btn") as HTMLButtonElement);
+	const showTransactionDiv = document.getElementById("show-transaction") as HTMLDivElement;
 
-	//Display Details 
-	const balancePlaceholder = (document.getElementById
-	("temporary-balance-placeholder") as HTMLParagraphElement);
-	const incomePlaceholder = (document.getElementById
-	("temporary-income-placeholder") as HTMLParagraphElement);
-	const expensePlaceholder = (document.getElementById
-	("temporary-expense-placeholder") as HTMLParagraphElement);
+	let transactions: Transaction[] = JSON.parse(localStorage.getItem("transactions") || "[]");
 
-	//Transactions Array
-	let transactions : Transaction[] = [];
+	function noTransactionMessage() : void {
+		const noTransaction = document.createElement("h3");
+		noTransaction.className = "text-gray-500 text-left font-bold";
+		noTransaction.textContent = "No Transaction";
+		showTransactionDiv.appendChild(noTransaction);
+	}
 
-
-	function addTransaction() : void {
+	function addTransaction(): void {
 		const enteredDescription = inputDescription.value.trim();
-		const enteredAmount =  parseInt(inputAmount.value);
+		const enteredAmount = parseInt(inputAmount.value);
 		const enteredType = inputType.value;
-		console.log("Entered type is: ",enteredType);
-		
-		
-		const newTransaction : Transaction = {
-			transactionDescription : enteredDescription,
-			transactionAmount : enteredAmount,
+
+		console.log("Entered type is:", enteredType);
+
+		if (!enteredDescription || !enteredAmount || !enteredType) {
+			alert("Please Add the required Fields");
+			return;
+		}
+
+		const newTransaction: Transaction = {
+			transactionDescription: enteredDescription,
+			transactionAmount: enteredAmount,
 			transactionType: enteredType === "income" ? "Income" : "Expense",
+			transactionId : Date.now()
 		};
 
 		transactions.push(newTransaction);
+		saveTransaction();
 		console.log(transactions);
 
-		if ((!enteredDescription || !enteredAmount || !enteredType)) {
-			alert("Please Add the required Fields");
-			return;
-		} else {
-			displayTransactionDetails();
-		}
+		displayTransactionDetails();
 
 		inputDescription.value = "";
 		inputAmount.value = "";
 	}
 
-	function displayTransactionDetails() : void {
+	function displayTransactionDetails(): void {
+		showTransactionDiv.innerHTML = "";
+
 		let income = 0;
 		let expense = 0;
 
-		transactions.forEach((transaction) => {
-			if (transaction.transactionType === "Income") {
-				income += transaction.transactionAmount;
-			} else {
-				expense += transaction.transactionAmount;
+		const transactionList = document.createElement("ul");
+		transactionList.className = "w-full bg-white shadow-md rounded-2xl p-4";
+
+		const title = document.createElement("h3");
+		title.className = "font-bold text-gray-800 text-xl mb-4";
+		title.textContent = "Transactions";
+		transactionList.appendChild(title);
+
+		if (transactions.length > 0) {
+			transactions.forEach((transaction) => {
+				const li = document.createElement("li");
+				li.className = "py-4 px-6 flex items-center justify-between border-b border-gray-200 last:border-none";
+
+				const desc = document.createElement("h4");
+				desc.className = "font-semibold text-gray-800";
+				desc.textContent = transaction.transactionDescription;
+
+				const amount = document.createElement("p");
+				amount.className =
+					transaction.transactionType === "Income"
+						? "text-green-600 font-medium"
+						: "text-red-600 font-medium";
+				amount.textContent = `₹${transaction.transactionAmount}`;
+
+				const type = document.createElement("p");
+				type.className = "text-gray-500 italic";
+				type.textContent = transaction.transactionType;
+
+				const deleteBtn = document.createElement('button');
+				deleteBtn.className = "delete-btn p-3 bg-red-500 rounded-md text-white font-medium cursor-pointer hover:bg-red-400 transition-colors duration-400";
+				deleteBtn.textContent = `Delete`;
+				deleteBtn.setAttribute('data-id',transaction.transactionId.toString());
+
+
+				li.append(desc, amount, type,deleteBtn);
+				transactionList.appendChild(li);
+
+				if (transaction.transactionType === "Income") {
+					income += transaction.transactionAmount;
+				} else {
+					expense += transaction.transactionAmount;
+				}
+			});
+
+			showTransactionDiv.appendChild(transactionList);
+		}
+
+		incomePlaceholder.textContent = `+ ₹${income}`;
+		expensePlaceholder.textContent = `- ₹${expense}`;
+		balancePlaceholder.textContent = `₹${income - expense}`;
+
+		//delete functionality
+		showTransactionDiv.addEventListener('click', (e) => {
+			const targetedListEvent = e.target as HTMLElement;
+			if (targetedListEvent.tagName === "BUTTON" && targetedListEvent.classList.contains("delete-btn")) {
+				const id = Number(targetedListEvent.getAttribute('data-id'));
+				if(!id) return;
+				transactions = transactions.filter((transaction) => transaction.transactionId !== id);
+				saveTransaction();
+				displayTransactionDetails();
+				if (transactions.length === 0) {
+					noTransactionMessage();
+					return;
+				} 
 			}
 		});
-
-		incomePlaceholder.textContent = `+ ${income}`;
-		expensePlaceholder.textContent = `${expense}`;
-		balancePlaceholder.textContent = `${income - expense}`;
 	}
 
-	addTransactionBtn.addEventListener('click',addTransaction);
+	function saveTransaction() : void{
+		localStorage.setItem('transactions',JSON.stringify(transactions));
+	}
 
-	
+	addTransactionBtn.addEventListener("click", addTransaction);
 
+	filterBtn.addEventListener('click',filterTransactions);
 
-
-
-
-
-})
+	//Show No Transaction Message By Default
+	if (transactions.length === 0) {
+		noTransactionMessage();
+	} else {
+		displayTransactionDetails();
+	}
+});
